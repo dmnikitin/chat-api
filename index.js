8b7a1a7be0f15d3ff6be4359900129aaf8fca891
Dmitry <joydnby@gmail.com>
2019-09-14 23:25:42 +0300

    feat: add signup handler

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const port = process.env.PORT || 8000;
@@ -10,8 +9,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => {
+//validator
+const requestUserCheck = req => {
+    const { username, password } = req.body.user;
+    if (username && password) {
+        return Promise.resolve({ username, password })
+    }
+};
 
+//signup handler
+
+app.post('/signup', (req, res, next) => {
+    //1_validate req data
+    requestUserCheck(req)
+        //2_ get from mongo
+        .then(json => getFromMongo(User, { username: json.username }))
+        //3_if no user in mongo - create one
+        .then(user => {
+            if (!user) {
+                // todo: const registrationDate  - create func for date
+                // create User mongo model
+                const newUser = new User({
+                    username: req.body.user.username,
+                    password: req.body.user.password,
+                    //registrationDate
+                })
+                return newUser.save();
+            }
+            return Promise.reject({ message: 'User already exists' })
+        })
+        .then(json => json
+            ? res.json({
+                success: true,
+                message: 'User has been successfully registered'
+            })
+            : Promise.reject({ message: 'Failed to register' }))
+        .catch(err => console.log(err.message))
 })
+
+
+//login
+
+
+//auth
+
+
+//logout
+
+
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}/`))
