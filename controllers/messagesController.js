fa6b1c0cb8fc3de33e008ed6fd30c03091b8ff3a
Dmitry <joydnby@gmail.com>
2019-09-15 14:25:41 +0300

    feat: add addMessage controller func

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -16,3 +16,7 @@ const addMessageController = (req, res, next) =>
         const newMessage = new Message({ username: message.username, content: message.content, date });
         return newMessage.save()
     })
+    .then(() => getFromMongo(Message))
+    .then(messages => messages && messages.length ? messages : Promise.reject({ message: 'There is no messages yet' }))
+    .then(messages => res.json({ success: true, messages }))
+    .catch(err => errorHandler(err, res, next))
