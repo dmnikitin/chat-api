eb73eb6e4b8cfb24b0ae57b0c7e6bc365877f7ec
Dmitry <joydnby@gmail.com>
2019-09-15 14:10:20 +0300

    feat: add message mongodb Model

---
+++ models/messageModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    content: { type: String, required: true },
+    date: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Message', messageSchema);
