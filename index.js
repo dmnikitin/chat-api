db238e3a3f8b2fd2632ea25aba2a98f24a8527d2
Dmitry <joydnby@gmail.com>
2019-09-16 22:48:19 +0300

    feat: add socket.io to index.js file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { MONGO_URI, port } = dotenv.config().parsed;
+const io = require('socket.io');
+
 const routes = require('./routes/index');
 
 const app = express();
+const server = http.createServer(app);
+const { MONGO_URI, port } = dotenv.config().parsed;
+
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', routes);
+
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
@@ -18,4 +24,5 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect(MONGO_URI);
 
 
+io.listen(server);
 app.listen(port, () => console.log(`Listening on http://localhost:${port}/`))
